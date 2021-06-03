0764539f1da8940588aeb1249125f63b2597ba52
Sayuj Nath <sayuj.nath@canditude.com>
2021-06-03 21:49:41 +1000

    Run server after install in  appspec.yml file

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
-PORT=15000 pm2 start /usr/local/share/applications/simplenode/app.js --name "simplenode"
+PORT=13000 pm2 start /usr/local/share/applications/simplenode/app.js --name "simplenode"
 pm2 save
 sudo env PATH=$PATH:/usr/local/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user
